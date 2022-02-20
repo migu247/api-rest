6edc3e739526a81b38332642ea0ed06db664aa55
migu247 <mafg17@alu.ua.es>
2022-02-21 00:53:15 +0100

    Incorporamos Nodemon y Morgan

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict'
-
-const port = process.env.PORT || 8888; 
- 
-const express = require('express'); 
-const app = express(); 
- 
- 
-app.get('/hola/:unNombre', (req, res) => { 
-    res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` }); 
-    }); 
-     
-    app.listen(port, () => { 
-      console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`); 
-    });
+const port = process.env.PORT || 3000;
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+// Declaramos los middleware
+app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
+// Declaramos el API
+app.get('/hola/:name', (req, res) => {
+res.status(200).send({mensaje: `¡Hola ${req.params.name} desde SD con JSON!`});
+});
+app.listen(port, () => {
+console.log((`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`));
+});
