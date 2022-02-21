0fe90186b17f5338b15bdb31be234314b1fd9ee8
migu247 <mafg17@alu.ua.es>
2022-02-21 01:19:55 +0100

    Codigo mejorado

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 'use strict'
+
 const port = process.env.PORT || 3000;
+
 const express = require('express');
 const logger = require('morgan');
+
 const app = express();
+
 // Declaramos los middleware
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // Implementamos el API RESTFul a través de los métodos
 app.get('/api/products', (req, res) => {
-res.status(200).send({products: []});
+res.status(200).send({products: [ {}, {}, {} ]});
 });
+
 app.get('/api/products/:productID', (req, res) => {
-res.status(200).send({products: `${req.params.productID}`});
+  const ID = req.params.productID;
+
+res.status(200).send(
+  {
+    _id: `${ID}`,
+    name: 'Mesa de oficina'
+  });
 });
+
 app.post('/api/products', (req, res) => {
-console.log(req.body);
-res.status(200).send({products: 'El producto se ha recibido'});
+  const miProducto = req.body;
+
+console.log(miProducto);
+res.status(200).send({ result: 'OK', product: miProducto});
 });
+
 app.put('/api/products/:productID', (req, res) => {
-res.status(200).send({products: `${req.params.productID}`});
+  const ID = req.params.productID;
+  const miProducto = req.body;
+
+res.status(200).send(
+  {
+    _id: `${ID}`,
+    name: 'Mesa de oficina'
+  });
 });
+
 app.delete('/api/products/:productID', (req, res) => {
-res.status(200).send({products: `${req.params.productID}`});
+  const ID = req.params.productID;
+
+res.status(200).send(
+  {
+    result: 'OK',
+    _id: `${ID}`
+  });
 });
+
 // Lanzamos nuestro servicio API
 app.listen(port, () => {
 console.log(`API REST ejecutándose en http://localhost:${port}/api/products`);
