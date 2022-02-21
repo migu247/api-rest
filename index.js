a09b0bc33b559010acfbd7e925e5241fe09fee3e
migu247 <mafg17@alu.ua.es>
2022-02-21 01:10:32 +0100

    API REST CRUD (sin base de datos)

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const logger = require('morgan');
 const app = express();
 // Declaramos los middleware
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
-// Declaramos el API
-app.get('/hola/:name', (req, res) => {
-res.status(200).send({mensaje: `¡Hola ${req.params.name} desde SD con JSON!`});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Implementamos el API RESTFul a través de los métodos
+app.get('/api/products', (req, res) => {
+res.status(200).send({products: []});
 });
+app.get('/api/products/:productID', (req, res) => {
+res.status(200).send({products: `${req.params.productID}`});
+});
+app.post('/api/products', (req, res) => {
+console.log(req.body);
+res.status(200).send({products: 'El producto se ha recibido'});
+});
+app.put('/api/products/:productID', (req, res) => {
+res.status(200).send({products: `${req.params.productID}`});
+});
+app.delete('/api/products/:productID', (req, res) => {
+res.status(200).send({products: `${req.params.productID}`});
+});
+// Lanzamos nuestro servicio API
 app.listen(port, () => {
-console.log((`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`));
+console.log(`API REST ejecutándose en http://localhost:${port}/api/products`);
 });
